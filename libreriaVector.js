2b1aca8d28c88be86314c9ecb30c33eaf55cb44b
nanosalc2 <nano011081@gmail.com>
2023-07-02 16:10:14 +0200

    primera subida

---
+++ libreriaVector.js
@@ -0,0 +1,67 @@
+const crearVector = (n, m) =>
+{
+    vector = [];
+     
+    for(let i = 0; i < n; i++)
+    {
+        vector.push(Math.round(Math.random() * m));
+    }
+    return vector;
+};
+
+ const sumaVector = (v1, v2) =>
+{
+   let v3 = [];
+    if(v1.length == v2.length)
+        {
+           for (let i = 0; i < v1.length; i++) 
+           {
+             v3[i] = v1[i] + v2[i];
+            
+           }
+        }
+    return v3;
+};
+
+ const productoNumeroVector = (n, v) =>
+{
+    let vectorProdu = [];
+    for (let i = 0; i < v.length; i++) 
+    {
+        vectorProdu[i] = n * v[i];
+    }
+    return vectorProdu;
+};
+
+ const restaVector = (v1, v2) =>
+{
+    let v3 = [];
+    if(v1.length == v2.length)
+        {
+           for (let i = 0; i < v1.length; i++) 
+           {
+             v3[i] = v1[i] - v2[i];
+            
+           }
+        }
+    return v3
+};
+
+const productoVector = (v1, v2) =>
+{
+  let v3 = [];
+    if(v1.length == v2.length)
+        {
+            for (let i = 0; i < v1.length; i++) 
+            {
+                v3[i] = v1[i] * v2[i];
+            
+            }
+        }
+    return v3
+     
+};
+
+const cuadrado = n => n * n;
+
+module.exports = {crearVector, sumaVector, productoNumeroVector, restaVector, productoVector, cuadrado};
